f3dae7026fb396df2ba0e87b64245f91014f5a95
HaqpaHR <pereverzev.viacheslav@gmail.com>
2021-04-04 17:50:04 +0300

    changed

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-class Clock {
-    constructor() {}
-    
-    update() {
-            let clock = document.getElementById('clock');
-            let date = new Date(); 
-            let hours = date.getHours();
-            if (hours < 10) hours = '0' + hours;
-            clock.children[0].innerHTML = hours;
-          
-            let minutes = date.getMinutes();
-            if (minutes < 10) minutes = '0' + minutes;
-            clock.children[1].innerHTML = minutes;
-          
-            let seconds = date.getSeconds();
-            if (seconds < 10) seconds = '0' + seconds;
-            clock.children[2].innerHTML = seconds;
-           
-            console.log(`${hours}:${minutes}:${seconds}`);
-    
-    }
+function time() {
+     const time = new Date();
+     let hours = time.getHours();
+     if (hours < 10) hours = '0' + hours;
 
-    start() {
-      this.update();
-      this.timer = setInterval(() => this.update(), 1000);
-    }
-  }
+     let minutes = time.getMinutes();
+     if (minutes < 10) minutes = '0' + minutes;
 
-let clo = new Clock()
-clo.start()
+     let seconds = time.getSeconds();
+     if (seconds < 10) seconds = '0' + seconds;
+
+     console.clear()
+     console.log(`${hours}:${minutes}:${seconds}`);
+
+     str = hours + ":" + minutes + ":" + seconds;
+
+     return str
+}
+
+function makeContainer(time) {
+    const timeDiv = document.createElement("div");
+    timeDiv.className = "clock";
+      for(let i = 0; i < time.length; i++){
+         const div = document.createElement("div");
+         div.className = "time" + i;
+         timeDiv.appendChild(div)
+      }
+    return timeDiv;
+}
+
+
+function timeShowOnScreen(time){
+    const clock = document.body.querySelector('.clock');
+    if(clock) {
+      const clockDivs = clock.querySelectorAll("div");
+      for(let i = 0; i < time.length; i++){
+        clockDivs[i].innerText = time[i];
+      }
+    }
+}
+
+document.body.appendChild(makeContainer(time()));
+
+setInterval(() => timeShowOnScreen(time()), 1000);
